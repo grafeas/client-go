278aa2fec5644364085abdee908d39627e5a0cee
Aysylu Greenberg <aysylu@google.com>
2019-06-25 17:10:40 -0400

    updated maven repo link to https

--- generate_client.go
+++ generate_client.go
@@ -2,7 +2,7 @@
 // and generates Go client library from the downloaded Swagger spec.
 
 //go:generate wget https://github.com/grafeas/grafeas/raw/master/proto/v1beta1/swagger/grafeas.swagger.json
-//go:generate wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.5/swagger-codegen-cli-2.4.5.jar -O swagger-codegen-cli.jar
+//go:generate wget https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.5/swagger-codegen-cli-2.4.5.jar -O swagger-codegen-cli.jar
 //go:generate java -jar ./swagger-codegen-cli.jar generate -i ./grafeas.swagger.json -l go -o 0.1.0 -c ./config.go.json
 
 package generate_client
